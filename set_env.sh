4e53249030a068e5f992f2a4e293a1461f14b311
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2020-12-25 22:29:09 +0200

    Initial Commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=192.168.1.100
+export POSTGRES_DB=udagram-postgres
+export AWS_BUCKET=udagram-monolith-to-microservices
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=anysecret
+export URL=http://localhost:8100
